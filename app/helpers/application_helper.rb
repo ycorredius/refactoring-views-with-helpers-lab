0f453fd5ed20c491f9c2f3c497cbe399d0584e5c
shaun <ycorredius@gmail.com>
2020-01-13 18:29:02 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def display_artist(song_obj)
+        if song_obj.artist && song_obj.artist.name != ""
+            link_to song_obj.artist.name, artist_path(song_obj.artist)
+        else
+            link_to "Add Artist", edit_song_path(song_obj)
+        end
+    end
 end
